5dd45e57aeec20a92555ca543462d2c577c0a6cb
Samuel Luis dos Santos <mucasantos@gmail.com>
2022-07-29 14:50:49 -0300

    Initial Commit

---
+++ RicknMorty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RicknMorty
+//
+//  Created by Samuel Santos on 29/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RicknMorty/RicknMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RicknMortyApp.swift
+//  RicknMorty
+//
+//  Created by Samuel Santos on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RicknMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
